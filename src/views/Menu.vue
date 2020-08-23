3a2ae23eb7de81b57f45ea8af1d94fe0fec7a95a
Jalex <ayalavalencia11@gmail.com>
2020-08-23 16:45:09 -0600

    Update Menu.vue

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -15,20 +15,132 @@
       </div>
     </div>
     <div class="content-foodies">
-
+      <div class="menu-items-filters">
+        <input class="search-food" type="text" placeholder="Busca tu platillo favorito..." />
+        <div class="show-categories">
+          <button
+            :class="['show-categories-button', selectFilters == false ? 'active-filter' : '']"
+            @click="selectFilter(false)"
+          >Todas</button>
+          <button
+            :class="['show-categories-button' , categorie.id == selectFilters ? 'active-filter' : '']"
+            @click="selectFilter(categorie.id)"
+            :key="categorie.id"
+            v-for="categorie in categories"
+          >{{categorie.name}}</button>
+        </div>
+      </div>
+      <div class="show-menu-card">
+        <div class="grid lg:grid-cols-4 sm:grid-cols-2 gap">
+          <!--start card items--->
+          <div :key="item.id" v-for="item in menu" class="max-w-sm rounded overflow-hidden shadow-lg mr-2 mb-5">
+            <img
+              class="w-full"
+              :src="item.image"
+              alt="Sunset in the mountains"
+            />
+            <div class="px-6 py-4">
+              <div class="font-bold mb-2">{{item.name}}</div>
+              <p
+                class="text-gray-700 text-base"
+              >{{item.description}}</p>
+              <div>
+              Price: {{item.price}}
+              Categoria: {{item.category}}
+            </div>
+            </div>
+            
+          </div>
+          <!--End card items--->
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
+  data() {
+    return {
+      categories: [],
+      selectFilters: false,
+      menu: [],
+    };
+  },
   mounted() {
     this.$store.state.classToActiveMenu = "text-white";
+    this.getCategories();
+    this.getMenu();
+  },
+  methods: {
+    getCategories: function () {
+      axios.get("https://api.elaniin.dev/api/categories").then((response) => {
+        this.categories = response.data.data;
+      });
+    },
+    selectFilter: function (receiveParam) {
+      this.selectFilters = receiveParam;
+      console.log(receiveParam);
+    },
+    getMenu: function () {
+      axios.get("https://api.elaniin.dev/api/menu?page=1").then((response) => {
+        this.menu = response.data.data;
+      });
+    },
   },
 };
 </script>
 
 <style scoped>
+.show-menu-card {
+  margin-left: 59px;
+  margin-right: 59px;
+}
+.active-filter {
+  background-size: 20px 8px !important;
+}
+.show-categories {
+  margin-left: 43px;
+}
+.show-categories-button {
+  margin-right: 40px;
+  outline: none;
+  font-family: Syne;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 17px;
+  line-height: 22px;
+  /*Effects*/
+  background: linear-gradient(to bottom, #ffd600 0%, #ffd600 100%);
+  background-position: 0 50%;
+  background-repeat: repeat-x;
+  background-size: 4px 0px;
+  transition: background-size 0.4s;
+}
+
+.show-categories-button:hover {
+  background-size: 20px 8px;
+}
+.search-food {
+  outline: none;
+  background: #f8f8f8;
+  border: 1px solid #c4c4c4;
+  box-sizing: border-box;
+  border-radius: 8px;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  padding-left: 63px;
+  width: 467px;
+}
+.menu-items-filters {
+  margin-left: 60px;
+  margin-right: 60px;
+  margin-top: 80px;
+  margin-bottom: 96px;
+  display: flex;
+  align-items: center;
+}
 .container-landing-menu {
   height: 100vh;
   display: flex;
